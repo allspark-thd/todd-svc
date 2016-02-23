029da138ba8526e57913e6d70ab45489f38e3404
Josh Quintana <josh_d_quintana@homedepot.com>
2016-02-23 15:28:48 -0500

    Add linting to build

--- src/__tests__/token-test.js
+++ src/__tests__/token-test.js
@@ -10,76 +10,77 @@ const SECRET_URL =
 	`${ process.env.VAULT_URL }/v1/secret/some-secret-namespace/db`;
 
 describe( ':space-id/:service-id', () => {
-			var server, url;
-			beforeEach( () => {
+	var server, url;
+	beforeEach( () => {
 		// initialize server
-				server = toddsvc();
-				url = 'http://localhost:' + server.address().port;
+		server = toddsvc();
+		url = 'http://localhost:' + server.address().port;
 	} );
-			afterEach( () => {
-				server.close();
+	afterEach( () => {
+		server.close();
 	} );
 
-			context( 'existing credentials', () => {
-				const valid = '/pcf-space-id/pcf-service-instance-id';
-
-				describe( 'POST', () => {
-											describe( 'body', () => {
-								describe( '`app_instance`', () => {
-									it( 'is required', () =>
-						rp( {
-											method: 'POST',
-											url: `${ url }${ valid }`,
-						} )
-						.then( expect.fail, response =>
-							expect( response.statusCode ).to.equal( 400 ) )
+	context( 'existing credentials', () => {
+		const valid = '/pcf-space-id/pcf-service-instance-id';
+		describe( 'POST', () => {
+			describe( 'body', () => {
+				describe( '`app_instance`', () => {
+					it( 'is required', () =>
+						rp( { method: 'POST', url: `${ url }${ valid }` } )
+						.then(
+							expect.fail,
+							res => expect( res.statusCode ).to.equal( 400 ) )
 					);
-									it( 'must be a string', () =>
+
+					it( 'must be a string', () =>
 						rp( {
-											method: 'POST',
-											url: `${ url }${ valid }`,
-											body: {
-												app_instance: {},
+							method: 'POST',
+							url: `${ url }${ valid }`,
+							body: {
+								app_instance: {},
 							},
 						} )
-						.then( expect.fail, response =>
-							expect( response.statusCode ).to.equal( 400 ) )
+						.then(
+							expect.fail,
+							response => expect( response.statusCode ).to.equal( 400 ) )
 					);
 
 
 
-									context( 'valid request', () => {
-										var validRequest, result;
-										beforeEach( () => {
-											validRequest = {
-												method: 'POST',
-												url: `${ url }${ valid }`,
-												body: {
-													'app_instance': 'aa-bb-cc-dd',
+					context( 'valid request', () => {
+						var validRequest, result;
+						beforeEach( () => {
+							validRequest = {
+								method: 'POST',
+								url: `${ url }${ valid }`,
+								body: {
+									app_instance: 'aa-bb-cc-dd',
 								},
-												resolveWithFullResponse: true,
-												json: true,
+								resolveWithFullResponse: true,
+								json: true,
 							};
-											return rp( validRequest )
+							return rp( validRequest )
 								.then( response => {
-													result = response;
+									result = response;
 								} );
 						} );
-										it( 'returns a 200', () => expect( result.statusCode ).to.equal( 200 ) );
-										it( 'returns json', () =>
-							expect( result.headers[ 'content-type' ] )
-								.to
-								.contain( 'application/json' ) );
 
-										describe( '`token`', () => {
-											it( 'is a string', () =>
+						it( 'returns a 200', () =>
+							expect( result.statusCode ).to.equal( 200 ) );
+
+						it( 'returns json', () =>
+							expect( result.headers[ 'content-type' ] )
+								.to.contain( 'application/json' ) );
+
+						describe( '`token`', () => {
+							it( 'is a string', () =>
 								expect( result.body.token )
 									.to.be.a( 'string' ) );
-											it( 'can be used against vault', () =>
+							it( 'can be used against vault', () =>
 								rp( {
-													url: SECRET_URL,
-													headers: {
-														'X-Vault-Token': result.body.token,
+									url: SECRET_URL,
+									headers: {
+										'X-Vault-Token': result.body.token,
 									},
 								} )
 							);
@@ -90,13 +91,6 @@ describe( ':space-id/:service-id', () => {
 
 				} );
 			} );
-
-			// it( 'has 200 status', () => rp( {
-			//		 url: validUrl,
-			//		 resolveWithFullResponse: true,
-			//	 } )
-			//	 .then( response => expect( response.statusCode ).to.equal( 200 ) )
-			// );
 		} );
 	} );
 } );
--- src/tokenRoute.js
+++ src/tokenRoute.js
@@ -2,54 +2,41 @@ import requestpromise from 'request-promise';
 import { Router } from 'express';
 
 const rp = requestpromise.defaults( {
-  json: true,
-  url: `${ process.env.VAULT_URL }/v1/auth/app-id/login`,
-  method: `POST`
+	json: true,
+	url: `${ process.env.VAULT_URL }/v1/auth/app-id/login`,
+	method: 'POST',
 } );
 
 const router = new Router();
 
 router.param( 'space', ( req, res, next, space ) => {
-  req.spaceid = space;
-  next();
-})
+	req.spaceid = space;
+	next();
+} );
 router.param( 'service', ( req, res, next, service ) => {
-  req.serviceid = service;
-  next();
-})
-router.post( '/:space/:service', (req, res, next) => {
-  if ( typeof req.body.app_instance !== 'string' ) {
-    res.status( 400 ).send( 'Expected `app_instance` string in body.' );
-    return;
-  }
-  rp( {
-    body: {
-      "app_id": req.spaceid,
-      "user_id": req.serviceid
-    }
-  } )
-  .then(responseBody => {
-    res.json({
-      token: responseBody.auth.client_token
-    });
-  }, vaultResp => vaultResp.pipe( res ) )
-});
+	req.serviceid = service;
+	next();
+} );
+router.post( '/:space/:service',
+	( req, res, next ) => {
+		if ( typeof req.body.app_instance !== 'string' ) {
+			const err = new Error( 'Expected `app_instance` string in body.' );
+			err.statusCode = 400;
+			res.status( 400 ).send( err );
+			return;
+		}
+		next();
+	},
+	( req, res, next ) =>
+		rp( {
+			body: {
+				app_id: req.spaceid,
+				user_id: req.serviceid,
+			},
+		} )
+		.then( responseBody =>
+			res.json( { token: responseBody.auth.client_token } ),
+			vaultResp => vaultResp.pipe( res ) )
+);
 
-
-/*
-router.param('id', function (req, res, next, id) {
-  console.log('CALLED ONLY ONCE');
-  next();
-});
-
-router.get('/user/:id', function (req, res, next) {
-  console.log('although this matches');
-  next();
-});
-
-router.get('/user/:id', function (req, res) {
-  console.log('and this matches too');
-  res.end();
-});
-*/
 export default router;
